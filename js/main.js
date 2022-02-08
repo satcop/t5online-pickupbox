98b614d50f81c8be4cb625a593132c2c51024dfc
PARKKYUTAE <satcop@naver.com>
2022-02-09 01:23:11 +0900

    이것저것 수정 커밋

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 $(function() {
 
+    //메뉴 토글
+    $('.bind-menu-button').on('click', function() {
+        $('body').addClass('show-side-menu');
+    });
+    $('.bind-close-button').on('click', function() {
+        $('body').removeClass('show-side-menu');
+    });
+
     $('.bind-add-order').on('click', function() {      
         $('.popup-order').show();
     });
-
     $('.bind-button-close').on('click', function() {
         $('.popup-order').hide();
     });
-
 
 });
